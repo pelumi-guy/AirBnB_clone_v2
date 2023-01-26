ac247bfbf175c939e896d6b90642455b60b35963
Pelumi <pelumi.olalekan.g@gmail.com>
2023-01-26 22:10:24 +0000

    task 3 pep8 validated

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -46,26 +46,26 @@ def do_deploy(archive_path):
     if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
     if sudo("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
+            format(name)).failed is True:
         return False
     if sudo("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
+            format(name)).failed is True:
         return False
     if sudo("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
+            format(file, name)).failed is True:
         return False
     if sudo("rm /tmp/{}".format(file)).failed is True:
         return False
     if sudo("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+            "/data/web_static/releases/{}/".format(name, name)).failed is True:
         return False
     if sudo("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
+            format(name)).failed is True:
         return False
     if sudo("rm -rf /data/web_static/current").failed is True:
         return False
     if sudo("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
+            format(name)).failed is True:
         return False
     return True
 

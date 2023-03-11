655ca0c28c9136fd81d75e030aad3a7130f991e4
Abiodun Shittu <jerrywizklay1@gmail.com>
2023-03-11 16:53:10 +0100

    States and State

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-"""Importing Flask to run the web app"""
-from flask import Flask, render_template
+"""Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /states: HTML page with a list of all State objects.
+    /states/<id>: HTML page displaying the given state with <id>.
+"""
 from models import storage
-from models.state import State
-
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
-
 
 @app.teardown_appcontext
 def close(self):
@@ -15,20 +18,22 @@ def close(self):
 
 
 @app.route('/states', strict_slashes=False)
-def state():
-    """Displays a html page with states"""
-    states = storage.all(State)
-    return render_template('9-states.html', states=states, mode='all')
 
-
-@app.route('/states/<id>', strict_slashes=False)
-def state_by_id(id):
-    """Displays a html page with citys of that state"""
-    for state in storage.all(State).values():
+@app.route("/states/<id>", strict_slashes=False)
+def states_id(id):
+    """Displays an HTML page with info about <id>, if it exists."""
+    for state in storage.all("State").values():
         if state.id == id:
-            return render_template('9-states.html', states=state, mode='id')
-    return render_template('9-states.html', states=state, mode='none')
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html")
 
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="5000")
+@app.teardown_appcontext
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
+
